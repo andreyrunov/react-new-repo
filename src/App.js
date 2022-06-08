da5ab84cc3bf316daef7e9e9b6050f4aecc2fa18
Andrey Runov <elefant86@bk.ru>
2022-06-08 17:51:50 +0300

    add sorting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import { setSelectionRange } from '@testing-library/user-event/dist/utils';
 import React, { useState } from 'react';
-import Counter from './Components/Counter';
-import PostItem from './Components/PostItem';
 import PostList from './Components/PostList';
 import './styles/App.css'
-import MyInput from './UI/input/MyInput';
-import MyButton from './UI/button/MyButton';
 import PostForm from './UI/PostForm';
+import MySelect from './UI/select/MySelect';
 
 function App() {
   const [posts, setPosts] = useState([
-      { id:1, title: 'Javascript', body: 'Description'},
-      { id:2, title: 'Javascript 2', body: 'Description'},
-      { id:3, title: 'Javascript 3', body: 'Description'},
+      { id:1, title: 'sd', body: 'sdrv'},
+      { id:2, title: 'fe', body: 'mhm'},
+      { id:3, title: 'kio', body: 'aqaa'},
   ]);
+
+  const [selectedSort, setSelectedSort] = useState('')
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -23,9 +21,26 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost}/>
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue='Сортировка'
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'},
+          ]}
+        />
+      </div>
       {posts.length !== 0
         ? <PostList remove={removePost} posts={posts} title='Список постов 1'/>
         : <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
